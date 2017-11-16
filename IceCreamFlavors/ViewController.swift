09fabcf1cdc3729adbf129c1d3492c440bb5c36e
Steven Lipton <shlomo626@gmail.com>
2017-11-16 10:37:26 -0600

    Initial writing of application

--- IceCreamFlavors/ViewController.swift
+++ IceCreamFlavors/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var flavorName: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    var index = 0
+    let flavors = ["Vanilla","Chocolate","Coconut","Green Tea","Butter Pecan","Hazelnut"]
+    
+    @IBAction func changeFlavor(_ sender: UIButton) {
+        index = (index + 1) % flavors.count
+        image.image = UIImage(named: flavors[index])
+        flavorName.text = flavors[index]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
